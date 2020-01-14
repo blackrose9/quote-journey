1944f52b5d279157af0880f180407ea95578ef75
blackrose9 <sarah.mwendwa@gmail.com>
2020-01-14 22:50:12 +0300

    Add form.component and form.component.html files Added form selector to quotes-thumbnail to display form declared form component in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesThumbnailComponent } from './quotes/quotes-thumbnail.component';
-import { QuoteDetailComponent } from './quotes/quotes-details/quotes-details.component';
+import { QuoteDetailComponent } from './quotes/quotes-details.component';
 import { DateCountPipe } from './date-count.pipe';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesThumbnailComponent,
     QuoteDetailComponent,
-    DateCountPipe
+    DateCountPipe,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes-form/quotes-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes-form',
+  templateUrl: './quotes-form.component.html'
+})
+export class QuotesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
