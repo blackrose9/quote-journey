b8a75b6b65536dee6fbf2cd153516b13ecd6a2f9
blackrose9 <sarah.mwendwa@gmail.com>
2020-01-15 09:21:51 +0300

    Push new quotes to array and display them thumbnail.component.ts comment out bugged code

--- src/app/quotes/quotes-thumbnail.component.ts
+++ src/app/quotes/quotes-thumbnail.component.ts
@@ -8,31 +8,38 @@ import { QuoteClass } from "../quote";
 
 export class QuotesThumbnailComponent{
    quotes:QuoteClass[]=[
-      {
-         id: 1, 
-         quoteSays: "Give yourself time", 
-         author: "UnknownAuthor",
-         postedBy: "Sarah",
-         votes: 0,
-         datePosted: "02/02/2020"
-      },
-      {
-         id: 2,
-         quoteSays: "Give yourself space",
-         author: "UnknownAuthor",
-         postedBy: "Sarah",
-         votes: 0,
-         datePosted: "02/02/2020"
-      },
-      {
-         id: 3,
-         quoteSays: "Give yourself freedom",
-         author: "UnknownAuthor",
-         postedBy: "Sarah",
-         votes: 0,
-         datePosted: "02/02/2020"
-      }
+      // {
+      //    id: 1, 
+      //    quoteSays: "Give yourself time", 
+      //    author: "UnknownAuthor",
+      //    postedBy: "Sarah",
+      //    votes: 0,
+      //    datePosted: "02/02/2020"
+      // },
+      // {
+      //    id: 2,
+      //    quoteSays: "Give yourself space",
+      //    author: "UnknownAuthor",
+      //    postedBy: "Sarah",
+      //    votes: 0,
+      //    datePosted: "02/02/2020"
+      // },
+      // {
+      //    id: 3,
+      //    quoteSays: "Give yourself freedom",
+      //    author: "UnknownAuthor",
+      //    postedBy: "Sarah",
+      //    votes: 0,
+      //    datePosted: "02/02/2020"
+      // }
    ];
+
+   addNewQuote(quote){
+      let quotesLength = this.quotes.length;
+      quote.id=quotesLength+1;
+      quote.completeDate = new Date(quote.completeDate)
+      this.quotes.push(quote)
+   }
 
    toggleDetails(index){
       this.quotes[index].showDetails = !this.quotes[index].showDetails;
